d136b22486d6317929a00d185ca0e37569891f1c
Jahed Naghipoor <jahednaghipoor1361@gmail.com>
2020-03-09 23:02:22 +0100

    Laravel Website1 Day1

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+class PageController extends Controller
+    {
+        public function about()
+        {
+            return 'about page';
+        }
+    }
